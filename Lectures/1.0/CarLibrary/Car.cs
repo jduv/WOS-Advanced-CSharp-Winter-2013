3ac4a87564a4bdc76c9ee569976f8312f90c53bd
Jeremy Duvall <matchless.tone@gmail.com>
2013-11-14 19:34:59 -0500

    Committing demo code.

---
+++ Lectures/1.0/CarLibrary/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public enum Direction
+    {
+        NORTH, EAST, SOUTH, WEST
+    }
+
+    public class Car : ICar, IEquatable<Car>
+    {
+        public void Move(Direction direction, int velocity)
+        {
+            switch (direction)
+            {
+                case Direction.NORTH:
+                    Console.WriteLine("Moving north by " + velocity + " units");
+                    break;
+                case Direction.SOUTH:
+                    Console.WriteLine("Moving south by " + velocity + " units");
+                    break;
+                case Direction.WEST:
+                    Console.WriteLine("Moving west by " + velocity + " units");
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public void Lock(double latitude, double longitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(Car other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lectures/1.0/CarLibrary/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public interface ICar
+    {
+        void Move(Direction direction, int velocity);
+        void Lock(double latitude, double longitude);
+    }
+}
