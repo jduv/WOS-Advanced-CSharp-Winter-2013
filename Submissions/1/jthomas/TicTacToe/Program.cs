2235b7d76b1b5fbe4b64d06cbf8098a5ec5c1599
Jeremy Duvall <matchless.tone@gmail.com>
2013-11-16 18:53:52 -0500

    copying assignment into johns folder

---
+++ Submissions/1/jthomas/TicTacToe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TicTacToe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var game = new TicTacToeGame();
+            game.Play();
+        }
+    }
+}
