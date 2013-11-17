5de954acf40e96fc6863407ac43c23eccf3c268f
Jeremy Duvall <matchless.tone@gmail.com>
2013-11-16 19:04:55 -0500

    Committing assignments to submissions folder.

---
+++ Submissions/1/dsinclair/Problems/MutablePointStruct.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public struct MutablePointStruct
+    {
+        #region Fields & Constants
+
+        private double x;
+        private double y;
+
+        #endregion
+
+        #region Constructors & Destructors
+
+        public MutablePointStruct(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public double X
+        {
+            get
+            {
+                return this.x;
+            }
+
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+
+            set
+            {
+                this.y = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Submissions/1/dsinclair/Tests/Tests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace Tester
+{
+    [TestClass]
+    public class Tests
+    {
+        #region Problem 1
+
+        [TestMethod]
+        public void Problem1_PointClass_WithReturnValue()
+        {
+            // Instantiate the problem
+            var target = new Problem1();
+
+            // Set up the test
+            int x = 5;
+            int y = 10;
+            var pointClass = new PointClass(x, y);
+            var result = target.ReturnADoubledPointClass(pointClass);
+
+            // Assert truths.
+            Assert.IsNotNull(result);
+            Assert.AreNotSame(result, pointClass);
+            Assert.AreEqual(x * 2, pointClass.X);
+            Assert.AreEqual(y * 2, pointClass.Y);
+        }
+
+        [TestMethod]
+        public void Problem1_PointClass_SideAffecting()
+        {
+            // Instantiate the problem
+            var target = new Problem1();
+
+            // Set up the test
+            int x = 5;
+            int y = 10;
+            var pointClass = new PointClass(x, y);
+            target.DoubleTheTargetPointClass(pointClass);
+
+            // Assert truths.
+            Assert.IsNotNull(pointClass);
+            Assert.AreEqual(x * 2, pointClass.X);
+            Assert.AreEqual(y * 2, pointClass.Y);
+        }
+
+        [TestMethod]
+        public void Problem1_PointStruct_WithReturnValue()
+        {
+            // Instantiate the problem
+            var target = new Problem1();
+
+            // Set up the test
+            int x = 5;
+            int y = 10;
+            var pointStruct = new PointStruct(x, y);
+            var result = target.ReturnADoubledPointStruct(pointStruct);
+
+            // Assert truths.
+            Assert.IsNotNull(result);
+            Assert.AreNotSame(result, pointStruct);
+            Assert.AreEqual(x * 2, pointStruct.X);
+            Assert.AreEqual(y * 2, pointStruct.Y);
+        }
+
+        [TestMethod]
+        public void Problem1_PointStruct_SideAffecting()
+        {
+            // Instantiate the problem
+            var target = new Problem1();
+
+            // Set up the test
+            int x = 5;
+            int y = 10;
+            var pointStruct = new MutablePointStruct() { X = x, Y = y };
+            var result = target.DoubleTheTargetPointStruct(pointStruct);
+
+            // Assert truths.
+            Assert.AreSame(result, pointStruct);
+            Assert.AreEqual(x * 2, result.X);
+            Assert.AreEqual(y * 2, result.Y);
+        }
+
+        [TestMethod]
+        public void Problem1_FindTheBug()
+        {
+            var target = new Problem1();
+
+            int x = 5;
+            int y = 10;
+            var pointStruct = new MutablePointStruct() { X = x, Y = y };
+            var result = target.FindTheBug(pointStruct);
+
+            Assert.AreEqual(x * 2, pointStruct.X);
+            Assert.AreEqual(y * 2, pointStruct.Y);
+            Assert.AreNotSame(pointStruct, result);
+        }
+
+        #endregion
+    }
+}
