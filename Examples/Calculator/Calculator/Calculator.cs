03d044fb52f8b7fea0c872f85f050ccc375c5d59
Jeremy Duvall <matchless.tone@gmail.com>
2013-12-06 20:03:06 -0500

    committing calculator example

---
+++ Examples/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+namespace MyCalculator
+{
+    public class Calculator : ICalculator
+    {
+        #region Public Methods
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("Can't divide by zero!");
+            }
+
+            return x / y;
+        }
+
+        public double Pythagorus(int a, int b)
+        {
+            return  Math.Sqrt(a*a + b*b);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Examples/Calculator/CalculatorUnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculator;
+using System;
+
+namespace CalculatorUnitTests
+{
+    [TestClass]
+    public class CalculatorUnitTests
+    {
+        [TestMethod]
+        public void Add_ValidNumbers()
+        {
+            var target = new Calculator();
+            var ret = target.Add(1, 2);
+
+            Assert.AreEqual(3, ret);
+        }
+
+        [TestMethod]
+        public void Subtract_ValidNumbers()
+        {
+            var target = new Calculator();
+            var ret = target.Subtract(2, 1);
+
+            Assert.AreEqual(1, ret);
+        }
+        [TestMethod]
+        public void Multiply_ValidNumbers()
+        {
+            var target = new Calculator();
+            var ret = target.Multiply(2, 4);
+
+            Assert.AreEqual(8, ret);
+        }
+        [TestMethod]
+        public void Divide_ValidNumbers()
+        {
+            var target = new Calculator();
+            var ret = target.Divide(20, 2);
+
+            Assert.AreEqual(10, ret);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Divide_ByZero()
+        {
+            var target = new Calculator();
+            var ret = target.Divide(20, 0);
+
+            Assert.AreEqual(0, ret);
+        }
+
+        [TestMethod]
+        public void GetHypo()
+        {
+            var target = new Calculator();
+            var ret = target.Pythagorus(3, 4);
+
+            Assert.AreEqual(5, ret);
+        }
+    }
+}
